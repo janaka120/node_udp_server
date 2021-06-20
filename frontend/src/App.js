d7b158a62812d93fc56e0f7329ea1fe2f17986e4
janaka120 <janakastill@gmail.com>
2021-06-20 23:21:39 +0530

    Add frontend init commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
 import './App.css';
+// const { useState, useEffect, useRef } = React;
+// import { Breadcrumb, Button, Form, Col, } from 'react-bootstrap';
+// import { Collapse, Popover, Toast, Tooltip, Alert, Modal, Dropdown } from bootstrap;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    return (
+        <div className="wrapper">
+            <div className="main-panel">
+                <div>Main page</div>
+            </div>
+        </div>
+    )
+
+}
 export default App;
+// ReactDOM.render(<App/>, document.querySelector('#root'))
